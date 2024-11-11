dc92c4738da2b806170d3d1911d6e4aead9cbcd4
CectAss <npnbem.fm@gmail.com>
2024-11-11 16:32:43 +0300

    asd

--- src/main/java/com/example/TestR/TestRApplication.java
+++ src/main/java/com/example/TestR/TestRApplication.java
@@ -2,11 +2,14 @@ package com.example.TestR;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class TestRApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TestRApplication.class, args);
 	}
 
