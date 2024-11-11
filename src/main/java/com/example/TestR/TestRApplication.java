9573922f4579fe5af3360f119bdd763302ea9275
CectAss <npnbem.fm@gmail.com>
2024-11-11 16:03:50 +0300

    start

---
+++ src/main/java/com/example/TestR/TestRApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TestR/TestRApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
